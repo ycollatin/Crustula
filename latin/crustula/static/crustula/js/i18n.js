f7c9b855df2f4046e0875ef420626cb2a4fdd8cf
Georges Khaznadar <georgesk@debian.org>
2021-06-26 18:36:13 +0200

    added missing files

---
+++ latin/crustula/static/crustula/js/i18n.js
@@ -0,0 +1,31 @@
+var i18ndiv = document.querySelector("#i18n");
+var body = document.querySelector("body");
+
+var languages = {
+    fr: "French language",
+    en: "English language",
+    ru: "Russian language"
+}
+
+if (! i18ndiv) {
+    i18ndiv = document.createElement("div");
+    i18ndiv.setAttribute("id", "i18n");
+    var innerHTML = "";
+    for (lang in languages)  {
+	innerHTML += "<image src='/static/crustula/images/" + lang +
+	    ".png' alt='" + languages[lang] +
+	    "' onclick='preferLanguage(\"" + lang +
+	    "\")'/>";
+    }
+    i18ndiv.innerHTML = innerHTML;
+    body.insertBefore(i18ndiv, body.childNodes[0]);
+}
+
+/**
+ * select the preferred language
+ * this one will be kept in a cookie, with seven-days duration
+ **/
+function preferLanguage(lang) {
+    document.cookie = "preferred_language=" + lang + "; SameSite=Strict; max-age=604800";
+    document.location = "index.php";
+}
--- latin/latin/urls.py
+++ latin/latin/urls.py
@@ -1,4 +1,4 @@
-"""latin URL Configuration
+"""latin1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('crustula/', include('crustula.urls')),
     path('admin/', admin.site.urls),
+    path('crustula/', include('crustula.urls')),
 ]
