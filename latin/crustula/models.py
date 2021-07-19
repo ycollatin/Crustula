2b1e266e6a0b315463d9094e71ebc41e8291d3ab
Georges Khaznadar <georgesk@georges-pc>
2021-07-19 11:11:24 +0200

    ajout d'un support des SOV dans la BDD

--- latin/crustula/models.py
+++ latin/crustula/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+from .crustula.utils.i18n import *
+
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     classe = models.CharField(max_length=20)
@@ -66,4 +68,57 @@ class Gaffiot(models.Model):
              "genitif": gaf.genitif, "genre": gaf.genre} for
             gaf in Gaffiot.objects.filter(comment=comment)
         ]
-    
+
+class Sov(models.Model):
+    """
+    classe utile pour chaque crutula de type "sov"
+    """
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"SOV: {self.name}"
+
+class Ov(models.Model):
+    """
+    vocabulaire pour Sujet/Objet ; essentiellement des noms au nominatif
+    et à l'accusatif
+    """
+    latine  = models.CharField(max_length=255)
+    gallice = models.CharField(max_length=255)
+    sovs = models.ManyToManyField(Sov)
+
+    def __str__(self):
+        return f"{self.latine} : {self.galle} ({self.sovs.all()})"
+
+    @property
+    def nomLatine(self):
+        """
+        @return le nominatif, en latin
+        """
+        return self.latine.split("/")[0]
+
+    @property
+    def accLatine(self):
+        """
+        @return l'accusatif, en latin
+        """
+        return self.latine.split("/")[1]
+
+    @property
+    def nomGallice(self):
+        """
+        @return le nominatif, en "gaulois"
+        """
+        return _(self.gallice).split("/")[0]
+
+    @property
+    def accGallice(self):
+        """
+        @return l'accusatif, en "gaulois"
+        """
+        g = _(self.gallice).split("/")
+        if g[1]:
+            # la traduction est déclinée à l'accusatif
+            return g[1]
+        # pas de déclinaison : on renvoie le nominatif
+        return g[0]
